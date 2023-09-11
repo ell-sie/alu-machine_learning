0ffd4cfa94815e93faebb42025ffb92ee1658753
ll-sie <e.ndiramiye@alustudent.com>
2023-09-11 12:33:10 +0200

    shape

--- math/linear_algebra/2-size_me_please.py
+++ math/linear_algebra/2-size_me_please.py
@@ -2,12 +2,13 @@
 
 def matrix_shape(matrix):
     shape = []
-  
+
     while isinstance(matrix, list):
         shape.append(len(matrix))
         matrix = matrix[0] if matrix else None
- 
+
     return shape
+
 
 if __name__ == "__main__":
     matrix_shape_function = __import__('2-size_me_please').matrix_shape
