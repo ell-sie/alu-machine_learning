740fe9c5cad66233de7257241d472f36434e4d27
Elsie Agasaro Ndiramiye <e.ndiramiye@alustudent.com>
2023-09-24 16:10:48 +0200

    Update 10-matisse.py

--- math/calculus/10-matisse.py
+++ math/calculus/10-matisse.py
@@ -2,15 +2,25 @@
 """Translate mathematical operations into code"""
 
 
-"""Translate mathematical operations into code"""
-
 def poly_derivative(poly):
-    """Calculate the derivative of a polynomial"""
+    """calculates the derivative of a polynomial"""
+    # check type
     if not isinstance(poly, list):
         return None
+
+    # if less than 2 terms - derivative == 0
     if len(poly) == 1:
         return [0]
+
+    # derivative * each coefficient by its power
     derivative = [poly[i] * i for i in range(1, len(poly))]
+
     if len(derivative) == 0:
+        return None
+
+    # If the derivative is 0, return [0]
+    if all(c == 0 for c in derivative):
         return [0]
-    return [0] if all(derivative) == 0 else derivative
+
+    return derivative
+    
