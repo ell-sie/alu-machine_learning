78421a1047748ae736b037dcbbe993d291583f76
ll-sie <e.ndiramiye@alustudent.com>
2023-09-24 15:36:38 +0200

    calculus for dummies

--- math/calculus/9-sum_total.py
+++ math/calculus/9-sum_total.py
@@ -2,17 +2,12 @@
 """Translate mathematical operations into code"""
 
 def summation_i_squared(n):
-    # Check if n is a valid integer and
-    # greater than or equal to 1
+    # Check if n is a valid integer and greater 
+    # than or equal to 1
     if not isinstance(n, int) or n < 1:
         return None
 
-    # Initialize the sum to 0
-    total = 0
+    # Use the formula for the sum of squares
+    result = (n * (n + 1) * (2 * n + 1)) // 6
 
-    # Iterate from 1 to n, adding the square of each number
-    # to the total
-    for i in range(1, n + 1):
-        total += i**2
-
-    return total
+    return result
