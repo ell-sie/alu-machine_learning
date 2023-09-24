53987bcc525cb164674d349fc5795b8d7afb6325
Elsie Agasaro Ndiramiye <e.ndiramiye@alustudent.com>
2023-09-24 15:48:33 +0200

    Update 9-sum_total.py

--- math/calculus/9-sum_total.py
+++ math/calculus/9-sum_total.py
@@ -1,13 +1,9 @@
 #!/usr/bin/env python3
 """Translate mathematical operations into code"""
 
+
 def summation_i_squared(n):
-    # Check if n is a valid integer and greater 
-    # than or equal to 1
-    if not isinstance(n, int) or n < 1:
+    """calculates sum_{i=1}^{n} i^2"""
+    if type(n) != int or n < 1:
         return None
-
-    # Use the formula for the sum of squares
-    result = (n * (n + 1) * (2 * n + 1)) // 6
-
-    return result
+    return int(n * (n + 1) * (2 * n + 1) / 6)
