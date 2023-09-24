0a9c6c428b2b42ff46e31467c018f7398737e367
Elsie Agasaro Ndiramiye <e.ndiramiye@alustudent.com>
2023-09-24 16:08:10 +0200

    Update 10-matisse.py

--- math/calculus/10-matisse.py
+++ math/calculus/10-matisse.py
@@ -6,7 +6,9 @@ def poly_derivative(poly):
     """Calculate the derivative of a polynomial"""
     if not isinstance(poly, list):
         return None
-    if not len(polly) == 1
-        return None
+    if len(polly) == 1
+        return [0]
     derivative = [poly[i] * i for i in range(1, len(poly))]
+    if len(derivative) == 0
+        return [0]
     return [0] if all(derivative) == 0 else derivative
