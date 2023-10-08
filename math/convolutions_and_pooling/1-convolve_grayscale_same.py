48f772d90f3ecef28f6c5e2383db09d365723db1
ll-sie <e.ndiramiye@alustudent.com>
2023-10-08 22:26:38 +0200

    convolving and pooling

--- math/convolutions_and_pooling/1-convolve_grayscale_same.py
+++ math/convolutions_and_pooling/1-convolve_grayscale_same.py
@@ -1,11 +1,12 @@
 #!/usr/bin/env python3
 import numpy as np
+"""
+Perform a same convolution on grayscale images.
+"""
 
 
 def convolve_grayscale_same(images, kernel):
     """
-    Perform a same convolution on grayscale images.
-
     Args:
         images (numpy.ndarray): Grayscale images of shape (m, h, w).
         kernel (numpy.ndarray): Convolution kernel of shape (kh, kw).
