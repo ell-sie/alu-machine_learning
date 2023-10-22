0bc7fa68f5e688b3db90098ba14407c92ea5b2c0
ll-sie <e.ndiramiye@alustudent.com>
2023-10-22 23:36:39 +0200

    multivariate probability

--- math/multivariate_prob/0-mean_cov.py
+++ math/multivariate_prob/0-mean_cov.py
@@ -1,22 +1,28 @@
 #!/usr/bin/env python3
-
-import numpy as np
-
-def mean_cov(X):
-    """
+"""
     Calculate the mean and covariance of a data set.
 
     Args:
-        X (numpy.ndarray): A 2D numpy array of shape (n, d) containing the data set.
-            n is the number of data points, and d is the number of dimensions in each data point.
+        X (numpy.ndarray): A 2D numpy array of shape
+        (n, d) containing the data set.
+            n is the number of data points, and d is
+            the number of dimensions in each data point.
+"""
+import numpy as np
 
+
+def mean_cov(X):
+    """
     Returns:
-        mean (numpy.ndarray): A 2D numpy array of shape (1, d) containing the mean of the data set.
-        cov (numpy.ndarray): A 2D numpy array of shape (d, d) containing the covariance matrix of the data set.
+        mean (numpy.ndarray): A 2D numpy array of shape (1, d)
+        containing the mean of the data set.
+        cov (numpy.ndarray): A 2D numpy array of shape (d, d) containing the covariance
+        matrix of the data set.
 
     Raises:
         TypeError: If X is not a 2D numpy array.
-        ValueError: If n is less than 2, indicating that X must contain multiple data points.
+        ValueError: If n is less than 2, indicating
+        that X must contain multiple data points.
     """
     if not isinstance(X, np.ndarray) or X.ndim != 2:
         raise TypeError("X must be a 2D numpy.ndarray")
