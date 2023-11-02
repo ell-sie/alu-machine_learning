e9658e6441b8aae4038a7e0f4185615ae70d78a0
ll-sie <e.ndiramiye@alustudent.com>
2023-11-02 13:32:25 +0200

    likelihood

--- math/bayesian_prob/0-likelihood.py
+++ math/bayesian_prob/0-likelihood.py
@@ -1,43 +1,24 @@
 #!/usr/bin/env python3
-"""
-    Calculate the likelihood of obtaining certain
-     data given various hypothetical probabilities.
 
-    Parameters:
-        x (int): The number of patients that develop
-         severe side effects.
-        n (int): The total number of patients observed.
-        P (numpy.ndarray): A 1D numpy array containing the various
-         hypothetical probabilities of developing severe side effects.
-"""
 import numpy as np
+from scipy.special import comb
 
-
-def likelihood(x, n, P):
+def binomial_distribution(x, n, P):
     """
-    Calculate the likelihood of obtaining certain data given various hypothetical probabilities.
-
+    Calculate the binomial distribution given the number of successes (x), total trials (n), and success probability (P).
+    
     Parameters:
-        x (int): The number of patients that develop severe side effects.
-        n (int): The total number of patients observed.
-        P (numpy.ndarray): A 1D numpy array containing the various hypothetical probabilities of developing severe side effects.
+        x (int): The number of successes.
+        n (int): The total number of trials.
+        P (numpy.ndarray): A 1D numpy array containing success probabilities.
 
     Returns:
-        numpy.ndarray: A 1D numpy array containing the likelihood of obtaining the data (x, n) for each probability in P.
-
-    Exceptions:
-        ValueError:
-            - If n is not a positive integer.
-            - If x is not an integer that is greater than or equal to 0.
-            - If x is greater than n.
-            - If any value in P is not in the range [0, 1].
-        TypeError:
-            - If P is not a 1D numpy array.
+        numpy.ndarray: A 1D numpy array containing the probability of obtaining x successes for each probability in P.
     """
     if not isinstance(n, int) or n <= 0:
         raise ValueError("n must be a positive integer")
     if not isinstance(x, int) or x < 0:
-        raise ValueError("x must be an integer that is greater than or equal to 0")
+        raise ValueError("x must be a non-negative integer")
     if x > n:
         raise ValueError("x cannot be greater than n")
     if not isinstance(P, np.ndarray) or P.ndim != 1:
@@ -45,7 +26,11 @@ def likelihood(x, n, P):
     if not all(0 <= p <= 1 for p in P):
         raise ValueError("All values in P must be in the range [0, 1]")
 
-    binomial_coefficient = np.math.comb(n, x)
-    likelihoods = binomial_coefficient * P**x * (1 - P)**(n - x)
+    binomial_coefficient = comb(n, x)
+    binomial_probabilities = binomial_coefficient * P**x * (1 - P)**(n - x)
 
-    return likelihoods
+    return binomial_probabilities
+
+if __name__ == '__main':
+    P = np.linspace(0, 1, 11)
+    print(binomial_distribution(26, 130, P))
