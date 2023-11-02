0de806940d52adcbc640b79a9c70820ab8fdf4eb
ll-sie <e.ndiramiye@alustudent.com>
2023-11-02 16:06:20 +0200

    transpose matrix

--- math/linear_algebra/3-flip_me_over.py
+++ math/linear_algebra/3-flip_me_over.py
@@ -1,1 +1,19 @@
 #!/usr/bin/env python3
+def matrix_transpose(matrix):
+    # Check if the input matrix is empty
+    if not matrix:
+        return []
+
+    # Determine the number of rows and columns in the input matrix
+    num_rows = len(matrix)
+    num_columns = len(matrix[0])
+
+    # Create a new matrix to store the transpose
+    transpose = [[0] * num_rows for _ in range(num_columns)]
+
+    # Iterate through the input matrix and fill in the transpose
+    for i in range(num_rows):
+        for j in range(num_columns):
+            transpose[j][i] = matrix[i][j]
+
+    return transpose
