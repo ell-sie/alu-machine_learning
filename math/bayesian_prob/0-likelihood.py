df4f09b88b264df0e9bab5d017aeafab8d945f1f
ll-sie <e.ndiramiye@alustudent.com>
2023-11-02 12:41:08 +0200

    likelihood

--- math/bayesian_prob/0-likelihood.py
+++ math/bayesian_prob/0-likelihood.py
@@ -32,15 +32,19 @@ def likelihood(x, n, P):
     """
     if not isinstance(n, int) or n <= 0:
         raise ValueError("n must be a positive integer")
-    if not isinstance(x, int) or x > 0:
-        raise ValueError
-    ("x must be an integer that is greater than or equal to 0")
+    if not isinstance(x, int) or x < 0:
+        raise ValueError("x must be a non-negative integer")
     if x > n:
         raise ValueError("x cannot be greater than n")
     if not isinstance(P, np.ndarray) or P.ndim != 1:
         raise TypeError("P must be a 1D numpy.ndarray")
     if not all(0 <= p <= 1 for p in P):
         raise ValueError("All values in P must be in the range [0, 1]")
+    if abs(sum(P) - 1.0) > 1e-10:
+        raise ValueError
+    ("The sum of values in P must be approximately equal to 1.0")
+    if not np.all(P[:-1] <= P[1:]):
+        raise ValueError("P must be a sorted array")
 
     binomial_coefficient = np.math.comb(n, x)
     likelihoods = binomial_coefficient * P**x * (1 - P)**(n - x)
