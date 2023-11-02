1fd85e5048ef5d8fbe9db251a92ad0b20fa5c679
ll-sie <e.ndiramiye@alustudent.com>
2023-11-02 16:21:32 +0200

    add two matrices element-wise

--- math/linear_algebra/6-howdy_partner.py
+++ math/linear_algebra/6-howdy_partner.py
@@ -1,3 +1,13 @@
 #!/usr/bin/env python3
+"""
+Write a function def cat_arrays(arr1, arr2):
+that concatenates two arrays
+"""
+
+
 def cat_arrays(arr1, arr2):
+    """   
+You can assume that arr1 and arr2 are lists of ints/floats
+You must return a new list
+    """
     return arr1 + arr2
