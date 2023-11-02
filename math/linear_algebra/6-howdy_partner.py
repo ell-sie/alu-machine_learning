c457909bd1be56673de628dfcdf71c53b4c22711
ll-sie <e.ndiramiye@alustudent.com>
2023-11-02 16:22:23 +0200

    concatenate arrays

--- math/linear_algebra/6-howdy_partner.py
+++ math/linear_algebra/6-howdy_partner.py
@@ -7,7 +7,7 @@ that concatenates two arrays
 
 def cat_arrays(arr1, arr2):
     """   
-You can assume that arr1 and arr2 are lists of ints/floats
-You must return a new list
+-You can assume that arr1 and arr2 are lists of ints/floats
+-You must return a new list
     """
     return arr1 + arr2
