6e430c9930f104927126f23fcac432cc7f961cfd
ll-sie <e.ndiramiye@alustudent.com>
2023-11-02 14:14:42 +0200

    testing my answer

--- math/bayesian_prob/0-likelihood.py
+++ math/bayesian_prob/0-likelihood.py
@@ -23,8 +23,8 @@ def likelihood(x, n, P):
         x (int): The number of patients that develop severe side effects.
         n (int): The total number of patients observed.
         P (numpy.ndarray): A 1D numpy array containing
-         the various hypothetical
-         probabilities of developing severe side effects.
+         the various hypothetical probabilities
+        of developing severe side effects.
 
     Returns:
         numpy.ndarray: A 1D numpy array containing the likelihood
@@ -43,14 +43,16 @@ def likelihood(x, n, P):
         raise ValueError("n must be a positive integer")
     if not isinstance(x, int) or x < 0:
         raise ValueError(
-            "x must be an integerthat is greater than or equal to 0")
+            "x must be an integer that is greater than or equal to 0"
+            )
     if x > n:
         raise ValueError("x cannot be greater than n")
     if not isinstance(P, np.ndarray) or P.ndim != 1:
         raise TypeError("P must be a 1D numpy.ndarray")
     if any(p < 0 or p > 1 for p in P):
         raise ValueError(
-            "All values in P must be in the range [0, 1]")
+            "All values in P must be in the range [0, 1]"
+            )
 
     likelihoods = [comb(n, x) * (p ** x) * ((1 - p) ** (n - x)) for p in P]
 
