d01c93ddaf1cbe277b83f494b017b2b435118fc4
ll-sie <e.ndiramiye@alustudent.com>
2023-11-02 14:34:44 +0200

    testing my answer

--- math/bayesian_prob/1-intersection.py
+++ math/bayesian_prob/1-intersection.py
@@ -16,38 +16,6 @@ import numpy as np
 from scipy.special import comb
 
 
-def likelihood(x, n, P):
-    """
-    Returns:
-        numpy.ndarray: A 1D numpy array containing the likelihood
-         of obtaining the data (x, n) for each probability in P.
-
-    Exceptions:
-        ValueError:
-            - If n is not a positive integer.
-            - If x is not an integer that is greater than or equal to 0.
-            - If x is greater than n.
-            - If any value in P is not in the range [0, 1].
-        TypeError:
-            - If P is not a 1D numpy array.
-    """  
-    if not isinstance(n, int) or n <= 0:
-        raise ValueError("n must be a positive integer")
-    if not isinstance(x, int) or x < 0:
-        raise ValueError(
-        "x must be an integer that is greater than or equal to 0")
-    if x > n:
-        raise ValueError("x cannot be greater than n")
-    if not isinstance(P, np.ndarray) or P.ndim != 1:
-        raise TypeError("P must be a 1D numpy.ndarray")
-    if any(p < 0 or p > 1 for p in P):
-        raise ValueError(
-        "All values in P must be in the range [0, 1]")
-
-    likelihoods = [comb(n, x) * (p ** x) * ((1 - p) ** (n - x)) for p in P]
-
-    return np.array(likelihoods)
-
 def intersection(x, n, P, Pr):
     """
       Calculate the intersection of obtaining certain data
@@ -67,6 +35,8 @@ def intersection(x, n, P, Pr):
      considering the prior beliefs in Pr.     
 
     """  
+    # return np.array(likelihoods)
+
     if not isinstance(n, int) or n <= 0:
         raise ValueError("n must be a positive integer")
     if not isinstance(x, int) or x < 0:
@@ -84,17 +54,11 @@ def intersection(x, n, P, Pr):
         "All values in P must be in the range [0, 1]")
     if any(p < 0 or p > 1 for p in Pr):
         raise ValueError(
-        f"All values in Pr must be in the range [0, 1]")
+        "All values in Pr must be in the range [0, 1]")
     if not np.isclose(Pr.sum(), 1):
         raise ValueError("Pr must sum to 1")
 
-    likelihoods = likelihood(x, n, P)
+    likelihoods = [comb(n, x) * (p ** x) * ((1 - p) ** (n - x)) for p in P]
     intersection = likelihoods * Pr
 
     return intersection
-
-if __name__ == '__main__':
-    P = np.linspace(0, 1, 11)
-    Pr = np.ones(11) / 11
-    result = intersection(26, 130, P, Pr)
-    print(result)
