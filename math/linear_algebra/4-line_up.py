85ee5093d0daa50846aeca17d2ef120064970621
ll-sie <e.ndiramiye@alustudent.com>
2023-11-02 16:14:18 +0200

    add two arrays element-wise

--- math/linear_algebra/4-line_up.py
+++ math/linear_algebra/4-line_up.py
@@ -1,5 +1,16 @@
 #!/usr/bin/env python3
+"""
+Writing a that function def add_arrays(arr1, arr2):
+that adds two arrays element-wise
+"""
+
+
 def add_arrays(arr1, arr2):
+    """
+You can assume that arr1 and arr2 are lists of ints/floats
+You must return a new list
+If arr1 and arr2 are not the same shape, return None
+    """
     if len(arr1) != len(arr2):
         return None
     return [x + y for x, y in zip(arr1, arr2)]
