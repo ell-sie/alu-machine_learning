af41f07aa858c67c3fe89cd3101af8647d8a91a2
ll-sie <e.ndiramiye@alustudent.com>
2023-11-02 16:24:26 +0200

    concatenate arrays along specific axis

--- math/linear_algebra/7-gettin_cozy.py
+++ math/linear_algebra/7-gettin_cozy.py
@@ -1,5 +1,17 @@
 #!/usr/bin/env python3
+"""
+Write a function def cat_matrices2D(mat1, mat2, axis=0):
+that concatenates two matrices along a specific axis:
+"""
+
+
 def cat_matrices2D(mat1, mat2, axis=0):
+    """
+You can assume that mat1 and mat2 are 2D matrices containing ints/floats
+You can assume all elements in the same dimension are of the same type/shape
+You must return a new matrix
+If the two matrices cannot be concatenated, return None
+    """
     if axis == 0:
         if len(mat1[0]) != len(mat2[0]):
             return None
