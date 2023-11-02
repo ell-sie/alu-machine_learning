df7466c8fb34c2feb6f718a6db1750035c3c5f17
ll-sie <e.ndiramiye@alustudent.com>
2023-11-02 16:17:24 +0200

    add two matrices element-wise

--- math/linear_algebra/5-across_the_planes.py
+++ math/linear_algebra/5-across_the_planes.py
@@ -1,5 +1,20 @@
 #!/usr/bin/env python3
+"""
+function def add_matrices2D(mat1, mat2):
+that adds two matrices element-wise
+"""
+
+
 def add_matrices2D(mat1, mat2):
+    """
+You can assume that mat1 and mat2 are 2D matrices containing ints/floats
+You can assume all elements in the same dimension are of the same type/shape
+You must return a new matrix
+If mat1 and mat2 are not the same shape, return None
+    """
     if len(mat1) != len(mat2) or len(mat1[0]) != len(mat2[0]):
         return None
-    return [[mat1[i][j] + mat2[i][j] for j in range(len(mat1[0]))] for i in range(len(mat1))]
+    return [
+        [
+            mat1[i][j] + mat2[i][j] for j in range(len(mat1[0]))
+        ] for i in range(len(mat1))]
