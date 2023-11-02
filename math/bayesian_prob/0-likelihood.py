3adcb700bcb42e0595d21b8c0d041128f9a2a237
ll-sie <e.ndiramiye@alustudent.com>
2023-11-02 12:25:16 +0200

    likelihood

--- math/bayesian_prob/0-likelihood.py
+++ math/bayesian_prob/0-likelihood.py
@@ -1,17 +1,24 @@
 #!/usr/bin/env python3
 import numpy as np
+"""
+    Calculate the likelihood of obtaining certain
+     data given various hypothetical probabilities.
+
+    Parameters:
+        x (int): The number of patients that develop
+         severe side effects.
+        n (int): The total number of patients observed.
+        P (numpy.ndarray): A 1D numpy array containing the various
+         hypothetical probabilitiesof developing severe side effects.
+"""
 
 def likelihood(x, n, P):
     """
-    Calculate the likelihood of obtaining certain data given various hypothetical probabilities.
 
-    Parameters:
-        x (int): The number of patients that develop severe side effects.
-        n (int): The total number of patients observed.
-        P (numpy.ndarray): A 1D numpy array containing the various hypothetical probabilities of developing severe side effects.
 
     Returns:
-        numpy.ndarray: A 1D numpy array containing the likelihood of obtaining the data (x, n) for each probability in P.
+        numpy.ndarray: A 1D numpy array containing the
+        likelihood of obtaining the data (x, n) for each probability in P.
 
     Exceptions:
         ValueError: 
@@ -24,7 +31,7 @@ def likelihood(x, n, P):
     """
     if not isinstance(n, int) or n <= 0:
         raise ValueError("n must be a positive integer")
-    if not isinstance(x, int) or x < 0:
+    if not isinstance(x, int) or x >= 0:
         raise ValueError("x must be an integer that is greater than or equal to 0")
     if x > n:
         raise ValueError("x cannot be greater than n")
