4bbe89ee60a59294dc402f18d8bb0fb099662eeb
ll-sie <e.ndiramiye@alustudent.com>
2023-11-02 18:49:42 +0200

    transpose matrix

--- math/linear_algebra/11-the_western_exchange.py
+++ math/linear_algebra/11-the_western_exchange.py
@@ -1,3 +1,13 @@
 #!/usr/bin/env python3
+"""
+Write a function def np_transpose(matrix): that transposes matrix:
+"""
+
+
 def np_transpose(matrix):
+    """
+You can assume that matrix can be interpreted as a numpy.ndarray
+You are not allowed to use any loops or conditional statements
+You must return a new numpy.ndarray
+    """
     return matrix.T
