34148fd79ee1dca291b7f22ca33629ea368e0398
ll-sie <e.ndiramiye@alustudent.com>
2023-11-02 16:30:34 +0200

    matrix multiplication

--- math/linear_algebra/8-ridin_bareback.py
+++ math/linear_algebra/8-ridin_bareback.py
@@ -1,5 +1,4 @@
 #!/usr/bin/env python3
-import numpy as np
 """
 Write a function def mat_mul(mat1, mat2): that performs matrix multiplication
 """
@@ -14,4 +13,4 @@ If the two matrices cannot be multiplied, return None
     """
     if len(mat1[0]) != len(mat2):
         return None
-    return np.dot(mat1, mat2)
+    return mat1 * mat2
