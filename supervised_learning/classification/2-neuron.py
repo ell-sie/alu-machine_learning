1533bbe81b5fe6547d7a31e935448ddcc94295c3
ell-sie <e.ndiramiye@alustudent.com>
2024-01-18 11:52:28 +0200

    performed forward propagation

--- supervised_learning/classification/2-neuron.py
+++ supervised_learning/classification/2-neuron.py
@@ -1,91 +1,13 @@
-# #!/usr/bin/env python3
-# """
-# a class Neuron that defines a single neuron
-# perfoming binary classification
-# """
-# import numpy as np
-
-
-# class Neuron:
-#     """a class Neuron that defines a single neuron
-#     perfoming binary classification
-#     """
-#     def __init__(self, nx):
-#         """
-#         Initializes a Neuron instance.
-
-#         Parameters:
-#         - nx (int): Number of input features to the neuron.
-
-#         Raises:
-#         - TypeError: If nx is not an integer.
-#         - ValueError: If nx is less than 1 (not a positive integer).
-#         """
-#         if not isinstance(nx, int):
-#             raise TypeError("nx must be an integer")
-#         if nx < 1:
-#             raise ValueError("nx must be a positive integer")
-
-#         # Initializing weights, bias, and activated output
-#         self.__W = np.random.randn(1, nx)
-#         self.__b = 0
-#         self.__A = 0
-
-#     @property
-#     def W(self):
-#         """
-#         Getter method for the weights vector.
-
-#         Returns:
-#         - numpy.ndarray: The weights vector.
-#         """
-#         return self.__W
-
-#     @property
-#     def b(self):
-#         """
-#         Getter method for the bias.
-
-#         Returns:
-#         - float: The bias value.
-#         """
-#         return self.__b
-
-#     @property
-#     def A(self):
-#         """
-#         Getter method for the activated output.
-
-#         Returns:
-#         - float: The activated output.
-#         """
-#         return self.__A
-    
-#     def forward_prop(self, X):
-#         """
-#         Performs forward propagation for the neuron.
-
-#         Parameters:
-#         - X (numpy.ndarray): Input data with shape (nx, m).
-
-#         Returns:
-#         - numpy.ndarray: The activated output (__A).
-#         """
-#         Z = np.dot(self.W, X) + self.b
-#         self.__A = 1 / (1 + np.exp(-Z))
-#         return self.__A
-
-
-# if __name__ == "__main__":
-#     np.random.seed(0)
-#     neuron = Neuron(784)
-#     neuron._Neuron__b = 1
-#     A = neuron.forward_prop(X)
-#     print(A)
 #!/usr/bin/env python3
+"""a class Neuron that defines a single neuron
+    perfoming forward propagation
+"""
 import numpy as np
 
 class Neuron:
+    """a class Neuron that defines a single neuron
+    perfoming forward propagation
+    """
     def __init__(self, nx):
         """
         Initializes a Neuron instance.
