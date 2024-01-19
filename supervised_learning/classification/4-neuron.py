06b9880263f4f816ae5400c9d4a8210da67edf02
ell-sie <e.ndiramiye@alustudent.com>
2024-01-19 16:53:56 +0200

    added evaluation

--- supervised_learning/classification/4-neuron.py
+++ supervised_learning/classification/4-neuron.py
@@ -88,7 +88,7 @@ class Neuron:
         m = Y.shape[1]
         cost = -1 / m * np.sum(Y * np.log(A) + (1 - Y) * np.log(1.0000001 - A))
         return cost
-  
+
     def evaluate(self, X, Y):
         """
         Evaluates the neuron's predictions.
