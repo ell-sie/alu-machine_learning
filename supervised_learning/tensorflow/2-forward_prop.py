3984b460da9fd97d2153d6d1817d79403bdf61c3
ell-sie <e.ndiramiye@alustudent.com>
2024-01-26 14:28:20 +0200

    forward prop

--- supervised_learning/tensorflow/2-forward_prop.py
+++ supervised_learning/tensorflow/2-forward_prop.py
@@ -9,8 +9,10 @@ def forward_prop(x, layer_sizes=[], activations=[]):
     """
     Function that returns the prediction of the network in tensor form
     x is the placeholder for the input data
-    layer_sizes is a list containing the number of nodes in each layer of the network
-    activations is a list containing the activation functions for each layer of the network
+    layer_sizes is a list containing the number
+    of nodes in each layer of the network
+    activations is a list containing the
+    activation functions for each layer of the network
     """
     create_layer = __import__('1-create_layer').create_layer
     prediction = x
