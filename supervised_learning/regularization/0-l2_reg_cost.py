6d826f5745df65d6284684ce47eb6938a873733a
ell-sie <e.ndiramiye@alustudent.com>
2024-02-02 15:26:14 +0200

    l2 rer cost

--- supervised_learning/regularization/0-l2_reg_cost.py
+++ supervised_learning/regularization/0-l2_reg_cost.py
@@ -4,12 +4,15 @@ Module for calculating the cost of a neural network with L2 regularization
 """
 import numpy as np
 
+
 def l2_reg_cost(cost, lambtha, weights, L, m):
     """
-    Function that calculates the cost of a neural network with L2 regularization
+    Function that calculates the cost of a
+    neural network with L2 regularization
     cost: the cost of the network without L2 regularization
     lambtha: the regularization parameter
-    weights: a dictionary of the weights and biases (numpy.ndarrays) of the neural network
+    weights: a dictionary of the weights
+    and biases (numpy.ndarrays) of the neural network
     L: the number of layers in the neural network
     m: the number of data points used
     Returns: the cost of the network accounting for L2 regularization
