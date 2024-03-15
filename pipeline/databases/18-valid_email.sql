071bf5617972851796aa2f04af66fe1772d96284
ell-sie <e.ndiramiye@alustudent.com>
2024-03-15 19:25:35 +0200

    email validation

---
+++ pipeline/databases/18-valid_email.sql
@@ -0,0 +1,10 @@
+-- Email validation
+DELIMITER $$
+CREATE TRIGGER reset_email
+BEFORE UPDATE ON users FOR EACH ROW
+BEGIN
+	IF OLD.email <> NEW.email THEN
+		SET NEW.valid_email = 0;
+	END IF;
+END $$
+DELIMITER ;
