16c8e53cc06dcaf0c49df6a2fab37a585d1f1383
ell-sie <e.ndiramiye@alustudent.com>
2024-06-30 09:10:45 +0200

    unsupervised learning

--- unsupervised_learning/clustering/7-maximization.py
+++ unsupervised_learning/clustering/7-maximization.py
@@ -19,9 +19,10 @@ def maximization(X, g):
         probabilities for each data point in each cluster
 
     return:
-        - pi: numpy.ndarray (k,) containing the updated priors for each cluster
-        - m: numpy.ndarray (k, d) containing updated centroid means for each cluster
-        - S: numpy.ndarray (k, d, d) updated covariance matrices for each cluster
+    - pi: numpy.ndarray (k,) containing the updated priors for each cluster
+    - m: numpy.ndarray (k, d) containing updated
+     centroid means for each cluster
+    - S: numpy.ndarray (k, d, d) updated covariance matrices for each cluster
     """
     if not isinstance(X, np.ndarray) or len(X.shape) != 2:
         return None, None, None
